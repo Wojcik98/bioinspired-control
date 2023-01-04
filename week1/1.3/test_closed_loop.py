fad2e15f73abb6cb4ebf4bc574bcb9c4becf2848
MazurkiewiczMatt <mateusz.a.mazurkiewicz@gmail.com>
2023-01-04 12:00:46 +0100

    Finish ex2

---
+++ week1/1.3/test_closed_loop.py
@@ -0,0 +1,95 @@
+import numpy as np
+import torch
+import torch_model
+import cv2
+import camera_tools as ct
+from FableAPI.fable_init import api
+from utils import initialize_camera, initialize_robot
+from time import sleep
+
+cam = ct.prepare_camera()
+print(cam.isOpened())  # False
+i = 0
+
+initialize_camera(cam)
+module = initialize_robot()
+
+# Set move speed
+speedX = 25
+speedY = 25
+api.setSpeed(speedX, speedY, module)
+
+# Set accuracy
+accurateX = 'HIGH'
+accurateY = 'HIGH'
+api.setAccurate(accurateX, accurateY, module)
+
+# TODO Load the trained model
+model = torch_model.MLPNet(4, 30, 2)
+model.load_state_dict(torch.load('trained_model.pth'))
+
+
+# dummy class for targets
+class CoordinateStore:
+    def __init__(self):
+        self.point = None
+        self.new = False
+
+    def select_point(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDBLCLK:
+            cv2.circle(frame, (x, y), 3, (255, 0, 0), -1)
+            self.point = [x, y]
+            self.new = True
+
+
+t = [api.getPos('X', module), api.getPos('Y', module)]
+
+# Instantiate class
+coordinateStore1 = CoordinateStore()
+
+# as alternative you can set prior targets
+
+cv2.namedWindow("test")
+cv2.setMouseCallback('test', coordinateStore1.select_point)
+
+while True:
+    frame = ct.capture_image(cam)
+    xy = ct.locate(frame)
+
+    cv2.imshow("test", frame)
+    k = cv2.waitKey(100)
+    if k == 27:
+        break
+
+    #print(coordinateStore1.point)
+    # get the prediction
+    if coordinateStore1.new:
+        print('NEW POINT!!!')
+        with torch.no_grad():
+            inp = torch.tensor([coordinateStore1.point]).float()
+            inp = (inp - 200) / 200
+
+            for i in range(6):
+                frame = ct.capture_image(cam)
+                cv2.imshow("test", frame)
+                cv2.waitKey(1)
+                xy = ct.locate(frame)
+                current_xy = torch.tensor([xy]).float()
+                current_xy = (current_xy - 200) / 200
+                m_input = torch.tensor([np.append(inp - current_xy, np.divide(t,90))]).float()
+                print(m_input)
+                outp = model(m_input) * 90
+                dt = outp.numpy()[0]
+                print(dt)
+                t = [t[0] + dt[0], t[1] + dt[1]]
+                api.setPos(t[0], t[1], module)
+                sleep(2)
+                while api.getMoving('X', module) and api.getMoving('Y', module):
+                    sleep(0.1)
+
+        coordinateStore1.new = False
+
+    # sleep(1.5)
+
+print('Terminating')
+api.terminate()
---
+++ week1/1.3/train_closed_loop.py
@@ -0,0 +1,102 @@
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch_model
+
+from tqdm import tqdm  # progress bar
+
+# Load data
+data = pickle.load(open("training_data.p", "rb"))
+
+angles = data[:, :2]
+angles /= 90.0
+end_pos = data[:, 2:]
+end_pos = (end_pos - 200) / 200
+
+angles_diff = np.array([[0, 0]])
+inp = np.array([[0, 0, 0, 0]])
+
+for i, angle in enumerate(angles):
+    for j, angle2 in enumerate(angles):
+        if i!=j:
+            angles_diff = np.concatenate((angles_diff, [angle - angle2]))
+            inp = np.concatenate((inp, [np.concatenate((end_pos[i] - end_pos[j], angle))]))
+
+print("Training set generated")
+# Use GPU?
+device = 'cpu'
+if torch.cuda.is_available():
+    print("Using GPU")
+    device = 'cuda'
+    # torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
+x = torch.from_numpy(inp).float()
+y = torch.from_numpy(angles_diff).float()
+print(y)
+# DONE split the training set and test set
+print(len(x.cpu()))
+train, test = torch.utils.data.random_split(range(len(x)), [0.8, 0.2])
+x_train, y_train = x[train.indices], y[train.indices]
+x_test, y_test = x[test.indices], y[test.indices]
+# Eventually normalize the data
+
+if device == 'cuda':
+    x = x.cuda()
+    y = y.cuda()
+
+# Define neural network - an example
+model = torch_model.MLPNet(4, 30, 2)
+# model = torch_model.Net(n_feature=2, n_hidden1=h, n_hidden2=h, n_output=2)
+# print(model)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+loss_func = torch.nn.MSELoss()
+num_epochs = 500
+
+# h = 16
+g = 0.8
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=g)
+
+l_vec = np.zeros(num_epochs)
+l_test_vec = np.zeros(num_epochs)
+
+for t in tqdm(range(num_epochs)):
+    # TODO Train network
+    model.train()
+    x, y = x_train, y_train
+    prediction = model(x)  # Forward pass prediction. Saves intermediary values required for backwards pass
+    loss = loss_func(prediction, y)  # Computes the loss for each example, using the loss function defined above
+    optimizer.zero_grad()  # Clears gradients from previous iteration
+    loss.backward()  # Backpropagation of errors through the network
+    optimizer.step()  # Updating weights
+    scheduler.step()
+
+    l = loss.data
+    if device == 'cuda':
+        l = l.cpu()
+    # print(l.numpy())
+    l_vec[t] = l.numpy()
+
+    model.eval()
+    y_pred = model(x_test)
+    test_loss = loss_func(y_pred, y_test)
+    l_test_vec[t] = test_loss
+
+# TODO Test the network
+model.eval()
+y_pred = model(x_test)
+test_loss = loss_func(y_pred, y_test)
+print(f'Test loss: {test_loss}')
+
+plt.plot(l_vec)
+plt.plot(l_test_vec)
+plt.legend(['train', 'test'])
+plt.yscale('log')
+
+torch.save(model.state_dict(), 'trained_model.pth')
+plt.show()
+
+# Parameter TIPS - Try
+# Adam with lr = 0.001
+# StepLR scheduler with step_size=100, and gamma = 0.999
+# Two hidden layers with 18 units each
